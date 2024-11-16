9d713db3498ce5d5d7e3eabe5fe06b7821f32b4c
Hexo13 <malzahnparker@gmail.com>
2024-11-16 11:05:59 -0600

    Updated

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -2,6 +2,43 @@
 import { RouterView } from "vue-router";
 </script>
 
+<script>
+    export default {
+        data() {
+            return {
+                query: ""
+            }
+        },
+        mounted() {
+            const connection = new BareMux.BareMuxConnection("/baremux/worker.js")
+            const wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
+            const bareUrl = (location.protocol === "https:" ? "https" : "http") + "://" + location.host + "/bare/"
+        },
+        methods: {
+            async search() {
+                let url = this.query; // if no periods are detected in the input, search google instead
+                let searchUrl = "https://www.google.com/search?q=";
+
+                if (!url.includes(".")) {
+                    url = searchUrl + encodeURIComponent(url);
+                } else {
+                    if (!url.startsWith("http://") && !url.startsWith("https://")) { // if no http or https is detected, add https automatically
+                        url = "https://" + url;
+                    }
+                }
+                if (!await connection.getTransport()) {
+                    await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
+                }
+                iframeWindow.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            }
+        }
+    }
+</script>
+
 <template>
-    <RouterView />
+    <iframe id="iframeWindow" class="iframeWindow"></iframe>
+    <form @submit.prevent="search">
+        <input type="text" id="urlInput" v-model="query" placeholder="Enter URL here">
+        <button type="submit" id="searchButton">Search Text</button>
+    </form>
 </template>
